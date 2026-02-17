e57bac2809fcfe3999a6c44a0877c11e6c4983a3
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2026-02-16 17:35:09 -0800

    Include esp_wifi.h and adjust WiFi channel settings
    Added esp_wifi.h include and modified setup for channel settings.

--- espnow.ino
+++ espnow.ino
@@ -5,6 +5,7 @@
   #include <espnow.h>
 #else // ESP32
   #include <WiFi.h>
+  #include "esp_wifi.h" 
   #include <esp_now.h>
 #endif
 
@@ -19,33 +20,39 @@
 #endif
 
 
+
 void process_message(const uint8_t *bssid, const uint8_t *dat, int len) {
   char mac[18];
   snprintf(mac, sizeof(mac), "%02x:%02x:%02x:%02x:%02x:%02x",bssid[0], bssid[1], bssid[2],bssid[3], bssid[4], bssid[5]);
-
-  Serial.print(mac);
-  Serial.print(" ");
-  Serial.println(len);
-}
+  Serial.printf("%s %d\n", mac, len);
+  }
 
 
 
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
+  delay(1000);
 
   esp_now_init();
   #ifdef ESP8266
+    Serial.println(WiFi.macAddress());
     WiFi.setSleepMode(WIFI_NONE_SLEEP);
+    wifi_set_channel(8);
     esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   #else // ESP32
     WiFi.setSleep(false);
+    esp_wifi_set_channel(8, WIFI_SECOND_CHAN_NONE);
   #endif
 
   esp_now_register_recv_cb(on_receive);
-
 }
 
 
 
 void loop() {}
+
+
+
+
+
