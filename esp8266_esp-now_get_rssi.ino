2ec6f5f082fac0df83ac0075e671ca0b82a4af3b
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2026-03-15 20:44:47 -0700

    Update esp8266_esp-now_get_rssi.ino

--- esp8266_esp-now_get_rssi.ino
+++ esp8266_esp-now_get_rssi.ino
@@ -1,40 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-struct RxControl {
-  signed   rssi         : 8;
-  unsigned rate         : 4;
-  unsigned is_group     : 1;
-  unsigned              : 1;
-  unsigned sig_mode     : 2;
-  unsigned legacy_length: 12;
-  unsigned damatch0     : 1;
-  unsigned damatch1     : 1;
-  unsigned bssidmatch0  : 1;
-  unsigned bssidmatch1  : 1;
-  unsigned MCS          : 7;
-  unsigned CWB          : 1;
-  unsigned HT_length    : 16;
-  unsigned Smoothing    : 1;
-  unsigned Not_Sounding : 1;
-  unsigned              : 1;
-  unsigned Aggregation  : 1;
-  unsigned STBC         : 2;
-  unsigned FEC_CODING   : 1;
-  unsigned SGI          : 1;
-  unsigned rxend_state  : 8;
-  unsigned ampdu_cnt    : 8;
-  unsigned channel      : 4;
-  unsigned              : 12;
-};  // 12 bytes total
-
 void OnDataRecv(uint8_t *mac, uint8_t *dat, uint8_t len) {
-  struct RxControl *rx = (struct RxControl *)(dat - 51); // offset always -51?
-  int rssi = rx->rssi - 100;  // subtract 100 for dBm
-
+  int rssi = (uint8_t)*(dat - 51) - 100; // Pointer offset always -51?, subtract 100 for dBm
   Serial.print("RSSI: ");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  Serial.println(rssi);
 }
 
 void setup() {
